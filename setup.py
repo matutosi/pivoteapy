2d484ed1fb276398fc5f0de510a56b715211927b
Toshikazu Matsumura <matutosi@gmail.com>
2025-03-21 05:28:35 +0900

    library

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pivotea',
+    version="0.1.1",
+    description="Create Pivot Table Easily",
+    author='Toshikazu Matsumura',
+    packages=find_packages(),
+    license='MIT'
+)
